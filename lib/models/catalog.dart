69316dd7d99ade842590f224975cc438b0c821a3
jaykhunt1591 <jaykhunt1591@gmail.com>
2022-03-08 12:21:45 +0530

    day 10 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,25 @@
+class item{
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+}
+
+
+final Products = [
+  item(
+    id:"1",
+    name:"i phone 12 pro max",
+    desc:"Apple i phone 12 generation",
+    price:999,
+    color:"#33505a",
+    image:"https://gadgetsleo.com/wp-content/uploads/2020/08/Apple-iPhone-12-Pro-Max-Gold-2.jpg"
+  ),
+
+];
+
+
